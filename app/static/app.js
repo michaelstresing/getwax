3e3e6a51b10f0d3b11b2d61f9e03983451524c4f
michaelstresing <mstresing@gmail.com>
2019-12-02 20:52:35 +0000

    Front-end improvements

--- app/static/app.js
+++ app/static/app.js
@@ -1,9 +1,26 @@
 const profurl = "/profile"
 
+// Setting custom color scheme
+const mainvuetify = new Vuetify({
+    themes: {
+      dark: {
+        primary: '#16161A',
+        accent: '#FFFFFE',
+        secondary: '#7F5AF0',
+        success: '#4CAF50',
+        info: '#2CB67D',
+        warning: '#FFEF61',
+        error: '#7D0000'
+      }
+    },
+  })
+
+Vue.use(mainvuetify)
+
 var vm = new Vue({
   el: '#app',
   delimiters: ["[[","]]"],    // To prevent conflicts with vue syntax and jinja {{ }}
-  vuetify: new Vuetify(),
+  vuetify: mainvuetify,
   data: {
     name: '',
     picture: '',
@@ -28,9 +45,9 @@ var vm = new Vue({
     },
 
     // To run getprofile on loading of page.
-    beforeMount(){
-      vm.getprofile()
-    }
+    // beforeMount(){
+    //   vm.getprofile()
+    // }
   })
 
 // To use FontAwesome icons. 
